5df5cb6cb21c72977b7c2188f78919290ad71635
qls051 <qls050100@naver.com>
2024-03-12 19:11:57 +0900

    주사위 세개

--- 알고리즘문제풀이/알고리즘문제풀이/알고리즘문제풀이.cpp
+++ 알고리즘문제풀이/알고리즘문제풀이/알고리즘문제풀이.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 
-    int n{};
-    cin >> n;
+    int num, num2, num3;
 
-    while (n!=0)
-    {
-        cout << n << '\n';
-        n--;
+    cin >> num >> num2 >> num3;
+
+    if (num != num2 && num2 != num3 && num != num3) {
+        int m;
+        m = max({ num,num2,num3 });
+        cout << m * 100;
     }
+    else if (num == num2 && num == num3) {
+        cout << 10000 + num * 1000;
+    }
+    else if (num == num2 || num == num3) {
+        cout << 1000 + num * 100;
+    }
+    else {
+        cout << 1000 + num2 * 100;
+    }
+
     return 0;
 }
